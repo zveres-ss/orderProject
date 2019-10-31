d4d7fdc44162598f1b28f8cd6fd5fa9262e46e76
zveres-ss <viruslviv@gmail.com>
2019-10-31 15:35:56 +0100

    enhance code with self link

--- src/main/java/com/lidl/shopping/service/controllers/CustomerController.java
+++ src/main/java/com/lidl/shopping/service/controllers/CustomerController.java
@@ -3,10 +3,15 @@ package com.lidl.shopping.service.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.hateoas.EntityModel;
 
 import com.lidl.shopping.logic.OrderService;
 import com.lidl.shopping.models.Customer;
@@ -21,12 +26,15 @@ public class CustomerController {
     
     @GetMapping(path = "/{customerId}/orders")
     public List<Order> getAllOrdersForCustomer(@PathVariable Integer customerId) {
-        return orderService.getOrdersForCustomer(customerId);
+        List<Order> customerOrders = orderService.getOrdersForCustomer(customerId);
+        
+        return customerOrders;
     }
     
     @GetMapping(path = "/{customerId}")
-    public Customer getCustomer(@PathVariable Integer customerId) {
-        return orderService.getCustomer(customerId);
+    public EntityModel<Customer> getCustomer(@PathVariable Integer customerId) {
+        Link link = linkTo(methodOn(CustomerController.class).getCustomer(customerId)).withSelfRel(); 
+        return new EntityModel<Customer>(orderService.getCustomer(customerId), link);
     }
 
 }
